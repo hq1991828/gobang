c0cd4809f883001941363d046db9b7236ceeb241
HQ <heqinihao1991@163.com>
2017-07-26 13:55:51 +0800

    棋子绘制

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ logo.src = "images/logo.png";
 logo.onload = function() {
     context.drawImage(logo,0,0,450,450);
     drawChessBoard();
+    oneStep(1,1,true);
+
 }
 
 var drawChessBoard = function() {
@@ -19,6 +21,23 @@ var drawChessBoard = function() {
         context.lineTo(435,15+i*30);
         context.stroke();
     }
+}
+
+var oneStep = function(i, j, me) {
+    context.beginPath();
+    context.arc(15 + i*30, 15 + j*30, 13, 0, 2 * Math.PI );
+    context.closePath();
+    var gradient = context.createRadialGradient(15 + i*30 + 2, 15 + j*30 -2, 13, 15 + i*30 + 2, 15 + j*30 -2, 0);
+    if(me){
+        gradient.addColorStop(0, "#0A0A0A");
+        gradient.addColorStop(1,"#636766");
+    } else {
+        gradient.addColorStop(0, "#d1d1d1");
+        gradient.addColorStop(1,"#f9f9f9");
+    }
+
+    context.fillStyle =  gradient;
+    context.fill();
 }
 
 
