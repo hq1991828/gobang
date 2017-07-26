595552f1093135213b5094d73ec9d35d9e16c791
HQ <heqinihao1991@163.com>
2017-07-26 14:18:47 +0800

    落子实现

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 var chess = document.getElementById('chess');
 var context = chess.getContext('2d');
+var me = true;
+var chessBoard = [];
+
+for(var i=0; i<15; i++) {
+    chessBoard[i] = [];
+    for(var j=0; j<15; j++) {
+        chessBoard[i][j] = 0;
+    }
+}
 
 context.strokeStyle = "#999";
 
@@ -8,7 +17,6 @@ logo.src = "images/logo.png";
 logo.onload = function() {
     context.drawImage(logo,0,0,450,450);
     drawChessBoard();
-    oneStep(1,1,true);
 
 }
 
@@ -40,4 +48,21 @@ var oneStep = function(i, j, me) {
     context.fill();
 }
 
+chess.onclick = function(e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var i = Math.floor(x/30);
+    var j = Math.floor(y/30);
+    if(chessBoard[i][j] ==0) {
+        oneStep(i,j,me);
+        if(me) {
+            chessBoard[i][j] = 1;
+        } else {
+            chessBoard[i][j] = 2;
+        }
+        me = !me;
+    }
+
+
+}
 
